a765539e92b5a5c9688b28a1705fb491df7f96aa
IT24102819 <it24102819@my.sliit.lk>
2025-03-23 23:28:58 +0530

    Created Reservation model

---
+++ src/main/java/model/Reservation.java
@@ -0,0 +1,59 @@
+package model;
+
+public class Reservation {
+    private String reservationId;
+    private String userId;
+    private String date;
+    private String time;
+    private int numberOfGuests;
+    private String status;
+
+    public Reservation(String reservationId, String userId, String date, String time, int numberOfGuests, String status) {
+        this.reservationId = reservationId;
+        this.userId = userId;
+        this.date = date;
+        this.time = time;
+        this.numberOfGuests = numberOfGuests;
+        this.status = status;
+    }
+
+    // Getters
+    public String getReservationId() {
+        return reservationId;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public String getDate() {
+        return date;
+    }
+    public String getTime() {
+        return time;
+    }
+    public int getNumberOfGuests() {
+        return numberOfGuests;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    // Setters
+    public void setReservationId(String reservationId) {
+        this.reservationId = reservationId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public void setTime(String time) {
+        this.time = time;
+    }
+    public void setNumberOfGuests(int numberOfGuests) {
+        this.numberOfGuests = numberOfGuests;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
